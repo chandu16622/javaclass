32b4903c8a2eb57fee5599e0a1374ccc668253e8
chandu16622 <devchandu52@gmail.com>
2025-09-21 10:38:40 +0530

    Update initialize.js

--- initialize.js
+++ initialize.js
@@ -8,22 +8,20 @@ class Student {
     }
 
     setFees(amount) {this.#fees = amount;}
-     getFees() { return this.#fees;}
-    //   #calculateDiscount() {return this.#fees * 0.1;}
-    //    calculateFinalFees() { return this.#fees - this.#calculateDiscount();}
-
+    getFees() { return this.#fees;}
+    #calculateDiscount() {return this.#fees * 0.1;}
+    calculateFinalFees() { return this.#fees - this.#calculateDiscount();}
     getDetails() {return `${this.name}, Age: ${this.age}, Course: ${this.course}`;}
 }   
    class SchoolStudent extends Student {
     getDetails() {
-        return `School Student: ${super.getDetails()} - Welcome to school!`;
-    }
-}
+    return `School Student: ${super.getDetails()} - Welcome to school!`;}
+   }
    class CollegeStudent extends Student {
     getDetails() {
-        return `College Student: ${super.getDetails()} - Welcome to college!`;
-    }
+    return `College Student: ${super.getDetails()} - Welcome to college!`;}
 }
+
 const s = new SchoolStudent("meera", 14, "Math");
 const c = new CollegeStudent("chinnu", 19, "Engineering");
 s.setFees(1000);
