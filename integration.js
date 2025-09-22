4a4f8f4f6b423a76d3f006a902d4e35add45b836
chandu16622 <devchandu52@gmail.com>
2025-09-22 11:01:50 +0530

    Update integration.js

--- integration.js
+++ integration.js
@@ -9,22 +9,23 @@ class Student {
   }
   setFees(fees) { this.#fees = fees; }
   getFees() { return this.#fees; }
-  #internalFeeCalc() { return this.#fees * 1.1; }
+  #internalFeeCalc() { return this.#fees }
   calculateFinalFees() { return this.#internalFeeCalc(); }
-  getDetails() { return `${this.name}, ${this.age}, ${this.course}`; }
 }
 
 class SchoolStudent extends Student {
-  getDetails() { return `School Student ${this.name} joined school.`; }
+  getDetails() { return `School Student name:${this.name},age:${this.age},course:${this.course},amount:${this.calculateFinalFees()} joined school`; }
 }
 
 class CollegeStudent extends Student {
-  getDetails() { return `College Student ${this.name} joined college.`; }
+  getDetails() { return `College Student name:${this.name},age:${this.age},course:${this.course},amount:${this.calculateFinalFees()} joined college.`; }
 }
 
-const s1 = new SchoolStudent("Amit", 15, "Math");
-const s2 = new CollegeStudent("Priya", 19, "Physics");
-s1.setFees(1000);
-s2.setFees(2000);
-console.log(s1.getDetails(), s1.calculateFinalFees());
-console.log(s2.getDetails(), s2.calculateFinalFees());
+const s1 = new SchoolStudent("Amit", 15, "10th");
+const s2 = new CollegeStudent("Priya", 19, "btech");
+s1.setFees(1400);
+s2.setFees(26000);
+console.log(s1.getDetails());
+console.log(s2.getDetails());
+
+
